02b23def4f3e04629289cdff3ed016787e14618f
jonathanalaksana <jonathanalaksana@gmail.com>
2021-09-14 14:36:11 -0700

    Added module docstring for entire project

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,11 +4,24 @@ TicTacToe Game and AI
 Started: 9/8/21
 Finished: 9/14/21
 
-Goal: Create a playable Tictactoe game with an unbeatable AI using the Minimax algorithm.
-    Implement with PyGame
-    use alpha-beta pruning to optimize
+Goal: Create a playable Tictactoe game with an unbeatable AI using the Minimax algorithm
+    and alpha-beta pruning.
 
+This project is a playable TicTacToe game with an unbeatable AI. You can also play
+in two player mode. This is my first AI project. I wanted to start off with something simple 
+first. I learned about game theory, minimax algorithm, and OOP.
 
+Instructions:
+ 1. Have Python 3.6 or later and the Pygame module installed
+ 2. Run the program
+ 3. Click if you want one player or two players
+ 4. Click which symbol player one will use
+ 5. Play TicTacToe by selecting a square and pressing space to add your symbol
+ 6. When there's a winner or draw, press space to play again.
+ 7. You can quit anytime with the X button on the top right.
+
+Resources:
+https://youtu.be/l-hh51ncgDI
 """
 from typing import List
 import pygame
