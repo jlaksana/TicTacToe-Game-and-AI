45ef27ddff4bc39f3a0d278d542a9d7e35bf90d1
jonathanalaksana <jonathanalaksana@gmail.com>
2021-09-14 14:28:51 -0700

    Added docstrings to all classes and methods

--- TicTacToe.py
+++ TicTacToe.py
@@ -10,6 +10,7 @@ Goal: Create a playable Tictactoe game with an unbeatable AI using the Minimax a
 
 
 """
+from typing import List
 import pygame
 import time
 from players import HumanPlayer, AIPlayer
@@ -17,23 +18,29 @@ from players import HumanPlayer, AIPlayer
 pygame.init()
 
 class TicTacToe:
-    N = 3
+    """TicTacToe class that holds the matrix representation of a board.
+    Can add a symbol, select, and draw onto pygame."""
+    N = 3   # Board dimension
     def __init__(self) -> None:
         self.board = [[" "] * TicTacToe.N for i in range(TicTacToe.N)]
-        self.num_empty = TicTacToe.N * TicTacToe.N
+        self.num_empty = TicTacToe.N * TicTacToe.N  # used to keep is_full constant time
         self.selected_pos = None
         
     def __repr__(self) -> str:
         return f"{self.board[0]}\n{self.board[1]}\n{self.board[2]}"
 
     def is_full(self) -> bool:
+        """Returns True if all slots are filled"""
         return self.num_empty == 0
 
     def cur_capacity(self) -> int:
+        """Returns the current amount of empty slots left as an integer"""
         return self.num_empty
 
-    def add_at_selected(self, symbol: str):
-        if self.is_full():
+    def add_at_selected(self, symbol: str) -> bool:
+        """Given a symbol, adds the symbol to the current select position.
+        Returns True if successfully added, else False"""
+        if self.is_full() or self.selected_pos is None:
             return False
 
         x, y = self.selected_pos    
@@ -43,10 +50,14 @@ class TicTacToe:
             return True
         return False
 
-    def add_at_i(self, symbol, i):
+    def add_at_i(self, symbol:str, i:int) -> bool:
+        """Given a symbol and an array index 0-8, adds the symbol at given index.
+        Used for AIPlayer minimax algorithm.
+        Returns True if successfully added, else False"""
         if self.is_full():
             return False
 
+        # Convert array index into matrix index
         x = i % 3
         y = i // 3
 
@@ -56,7 +67,9 @@ class TicTacToe:
             return True
         return False
 
-    def delete_symbol(self, i ):
+    def delete_symbol(self, i:int) -> None:
+        """Given a symbol and an array index 0-8, deletes the symbol at given index.
+        Used for AIPlayer minimax algorithm."""
         x = i % 3
         y = i // 3
         if (self.board[y][x] != " "):
@@ -64,7 +77,7 @@ class TicTacToe:
             self.num_empty += 1
 
     def has_winner(self) -> str:
-        """returns a string representing if there is a winner or not.
+        """Returns a string representing if there is a winner or not.
         "" - no winner
         "X" - X wins
         "O" - O wins
@@ -90,13 +103,18 @@ class TicTacToe:
 
         return ""
 
-    def copy(self):
+    def copy(self) -> object:
+        """Returns a copy of itself as another TicTacToe object.
+        Used for AIPlayer minimax algorithm to calculating without affecting
+        the current board in play"""
         copy = TicTacToe()
         copy.board = self.board.copy()
         copy.num_empty = self.num_empty
         return copy
 
-    def draw(self):
+    def draw(self) -> None:
+        """Draws board onto pygame screen.
+        """        
         gap = Screen.LENGTH / TicTacToe.N
         for i in range(1, TicTacToe.N):
             #draw horizontals
@@ -104,8 +122,10 @@ class TicTacToe:
             #draw verticals
             pygame.draw.line(Screen.WINDOW, (0,0,0), (i*gap,0), (i*gap,Screen.HEIGHT-50), 5)
 
+        # Draw slots
         for row in range(TicTacToe.N):
             for col in range(TicTacToe.N):
+                # Draw selected slot
                 if self.selected_pos == (col,row):
                     pygame.draw.rect(Screen.WINDOW, (155,207,225), (150 * col + 10, 150 * row + 10, 130, 130), 3)
 
@@ -116,7 +136,9 @@ class TicTacToe:
                 text_rect = text.get_rect(center=(75+150*col, 75+150*row))
                 Screen.WINDOW.blit(text,text_rect)
 
-    def select(self, position):
+    def select(self, position:tuple) -> None:
+        """Given a mouse position tuple (x,y), sets the selected slot to position
+        """
         if position[1] >= Screen.HEIGHT - 50:
             return
 
@@ -140,12 +162,15 @@ class Screen:
         """Fills the screen background with default background color"""
         Screen.WINDOW.fill(Screen.BG_COLOR)
     
-    def display_welcome():
+    def display_welcome() -> None:
+        """Displays 'Welcome' at the top of the screen."""
         text = Screen.SUBFONT.render("Welcome", True, (0,0,0))
         text_rect = text.get_rect(center=(Screen.LENGTH/2, 60))
         Screen.WINDOW.blit(text, text_rect)
 
-    def display_menu(opt1, opt2):
+    def display_menu(opt1:str, opt2:str) -> None:
+        """Given two options strings, displays them on the middle of the screen 
+        as a menu."""
         text = Screen.SUBFONT.render(opt1, True, (0,0,0))
         text_rect = text.get_rect(center=(Screen.LENGTH/2, Screen.HEIGHT/2-50))
         Screen.WINDOW.blit(text, text_rect)
@@ -153,7 +178,9 @@ class Screen:
         text_rect = text.get_rect(center=(Screen.LENGTH/2, Screen.HEIGHT/2+50))
         Screen.WINDOW.blit(text, text_rect)
 
-    def display_winner(winner:str):
+    def display_winner(winner:str) -> None:
+        """Displays game result in the middle of the screen. 
+        Asks if player wants to play again."""
         if winner == "D":
             msg = "DRAW"
         else:
@@ -166,12 +193,16 @@ class Screen:
         text_rect = text.get_rect(center=(Screen.LENGTH/2, Screen.HEIGHT/2+50))
         Screen.WINDOW.blit(text, text_rect)
 
-    def display_turn(player):
+    def display_turn(player: object) -> None:
+        """Given a Human or AI player who is the current player, displays their turn 
+        at the bottom right of the screen."""
         text = pygame.font.SysFont("arial", 30).render(player.symbol + "'s TURN", True, (0,0,0))
         text_rect = text.get_rect(center=(Screen.LENGTH-70, Screen.HEIGHT-25))
         Screen.WINDOW.blit(text,text_rect)
 
-    def display_score(scores):
+    def display_score(scores: List) -> None:
+        """Given a list with three values [P1, P2, Draws], displays
+        the score at the bottom left of the screen."""
         msg = f"P1: {scores[0]}  P2: {scores[1]}  D: {scores[2]}"
         text = pygame.font.SysFont("arial", 30).render(msg, True, (0,0,0))
         text_rect = text.get_rect(center=(110, Screen.HEIGHT-25))
@@ -179,6 +210,7 @@ class Screen:
     
 
 class Game:
+    """Game Class. Keeps track of players and score."""
     def __init__(self) -> None:
         self.player1 = HumanPlayer()
         self.player2 = None
@@ -186,7 +218,9 @@ class Game:
         self.other_player = None
         self.score = [0,0,0]  #[P1, P2, Draws]
 
-    def set_players(self):
+    def set_players(self) -> None:
+        """Provides the clickable functionality of the start menu to select
+        number of players."""
         while self.player2 is None:
             Screen.fill()
             Screen.display_welcome()
@@ -208,8 +242,10 @@ class Game:
             
         self.set_symbols()
 
-    def set_symbols(self):
-         while self.cur_player is None:
+    def set_symbols(self) -> None:
+        """Provides the clickable functionality of the start menu to select
+        symbol for player one."""
+        while self.cur_player is None:
             Screen.fill()
             Screen.display_welcome()
             Screen.display_menu("P1 choose:", "X or O")
@@ -236,11 +272,13 @@ class Game:
             pygame.display.update()
 
     def play(self, board):
+        """Lets current player play. Switches current player once play is made."""
         # Switch players if player finishes their turn
         if self.cur_player.play(board):
             self.cur_player, self.other_player = self.other_player, self.cur_player
 
-    def restart(self):
+    def restart(self) -> None:
+        """Resets current player to X player to start a new game."""
         if self.player1.symbol == "X":
             self.cur_player = self.player1
             self.other_player = self.player2
@@ -248,7 +286,8 @@ class Game:
             self.cur_player = self.player2
             self.other_player = self.player1
 
-    def update_score(self,winner):
+    def update_score(self,winner:str) -> None:
+        """Given 'X', 'O', or 'D', increments the score"""
         if winner == "D":
             self.score[2] += 1
         elif winner == self.player1.symbol:
@@ -256,7 +295,8 @@ class Game:
         else:
             self.score[1] += 1
 
-    def end_game(self, winner):
+    def end_game(self, winner:str) -> None:
+        """Functionality for end screen of the game. Let's user play again"""
         self.update_score(winner)
         while True:
             Screen.fill()
@@ -274,11 +314,16 @@ class Game:
             pygame.display.update()
 
 
-def game_loop():
+def game_loop() -> None:
+    """Main Game loop"""
     pygame.display.set_caption("TICTACTOE")
+    
+    # Instantiate board and game, set up players
     board = TicTacToe()
     g = Game()
     g.set_players()
+
+    # Main loop
     while True:
         Screen.fill()
         g.play(board)
@@ -286,8 +331,11 @@ def game_loop():
         Screen.display_turn(g.cur_player)
         Screen.display_score(g.score)
         pygame.display.update()
+        
+        # Check if game is over
         winner = board.has_winner()
         if winner:
+            # End game functions
             time.sleep(1)
             g.end_game(winner)
             board = TicTacToe()
--- players.py
+++ players.py
@@ -1,12 +1,19 @@
+"""
+TicTacToe Player Module
+Has a Player parent class, HumanPlayer, and AIPlayer"""
+
 import math
 import pygame
 import time
 
 class Player:
+    """Player parent class"""
     def __init__(self) -> None:
         pass
 
-    def set_symbol(self, symbol) -> None:
+    def set_symbol(self, symbol:str) -> None:
+        """sets the object's sumbol to parameter.
+        Param: String "X" or "O" """
         self.symbol = symbol
 
     def play(self, board) -> bool:
@@ -19,11 +26,16 @@ class HumanPlayer(Player):
     def set_symbol(self, symbol) -> None:
         return super().set_symbol(symbol)
 
-    def select(self,board):
+    def select(self,board) -> None:
+        """Given a TicTacToe board, gives the current mouse position
+        to select a slot."""
         pos = pygame.mouse.get_pos()
         board.select(pos)
 
     def play(self, board) -> bool:
+        """Given a board, handles if the player selects or confirms a play.
+        Returns True if player confirms play, else False.
+        """
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -46,48 +58,55 @@ class AIPlayer(Player):
         return super().set_symbol(symbol)
 
     def play(self, board) -> bool:
+        """Given a board, AI uses Minimax algorithm to play optimal move.
+        Returns True if player confirms play, else False."""
         copy = board.copy()
-        time.sleep(.5)
+        time.sleep(.5)  # Pause so game not updated instantly
         i = self.minimax(copy, -math.inf, math.inf, True, 0)
         board.add_at_i(self.symbol,i)
         return True
 
 
     def minimax(self, board, alpha: int, beta:int, maximizing:bool, depth: int) -> int:
+        """Minimax algorithm with alpha-beta pruning to play best move for TicTacToe.
+        No depth limit because Tictactoe is a simple game. Returns 
+        best move index on first call, the other calls returns the evaluated value of branch.
+        Params: TicTacToe object, max alpha, min beta, boolean representing turn, current depth.
+        """
         winner = board.has_winner()
         if winner:
             if winner == self.symbol:
-                return board.cur_capacity() + 1
+                return board.cur_capacity() + 1 # Uses capacity to favor wins with fewer turns
             elif winner != "D":
                 return -1 * (board.cur_capacity() + 1)
             else:
                 return 0
 
         if maximizing:
-            maxEval = -math.inf
+            max_eval = -math.inf
             best_idx = 0
             for i in range(9):
                 if not board.add_at_i(self.symbol, i): continue
                 eval = self.minimax(board, alpha, beta, False, depth +1)
-                if (eval > maxEval):
+                if (eval > max_eval):
                     best_idx = i
-                    maxEval = eval
+                    max_eval = eval
                 board.delete_symbol(i)
                 alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
             if depth == 0:
                 return best_idx
-            return maxEval
+            return max_eval
         else:
-            minEval = math.inf
+            min_eval = math.inf
             symbol = "O" if self.symbol == "X" else "X"
             for i in range(9):
                 if not board.add_at_i(symbol, i): continue
                 eval = self.minimax(board, alpha, beta, True, depth +1)
-                minEval = min(minEval, eval)
+                min_eval = min(min_eval, eval)
                 board.delete_symbol(i)
                 beta = min(beta, eval)
                 if beta <= alpha:
                     break
-            return minEval
+            return min_eval
